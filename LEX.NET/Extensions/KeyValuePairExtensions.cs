bf11fe500556fe06f50b60737dd8fc128eab4646
Markus Rezai <markus.rezai@gmail.com>
2018-02-05 13:45:06 +0100

    Added KeyValuePair extension deconstructor

---
+++ LEX.NET/Extensions/KeyValuePairExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Autrage.LEX.NET.Extensions
+{
+    public static class KeyValuePairExtensions
+    {
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+        {
+            pair.AssertNotNull();
+
+            key = pair.Key;
+            value = pair.Value;
+        }
+    }
+}
