a4f91ec87ce62d9a493c2b6d859cf9174fb92a05
Markus Rezai <markus.rezai@gmail.com>
2018-02-07 12:35:38 +0100

    Implemented clamp extension method
    Uses the nifty new ref extension method feature!

--- LEX.NET/Extensions/DoubleExtensions.cs
+++ LEX.NET/Extensions/DoubleExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Autrage.LEX.NET.Extensions
+﻿using System;
+
+namespace Autrage.LEX.NET.Extensions
 {
     public static class DoubleExtensions
     {
@@ -7,6 +9,41 @@
         public static bool IsBetween(this double value, double bound1, double bound2) =>
             bound1 < bound2 ? bound1 < value && value < bound2 : bound2 < value && value < bound1;
 
-        #endregion Methods
+        public static double Clamp(ref this double value, double bound1, double bound2)
+        {
+            double min = Math.Min(bound1, bound2);
+            double max = Math.Max(bound1, bound2);
+
+            if (value < min)
+            {
+                return value = min;
+            }
+            else if (value > max)
+            {
+                return value = max;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public static double Clamp01(ref this double value)
+        {
+            if (value < 0)
+            {
+                return value = 0;
+            }
+            else if (value > 1)
+            {
+                return value = 1;
+            }
+            else
+            {
+                return value;
+            }
+        }
     }
+
+    #endregion Methods
 }
