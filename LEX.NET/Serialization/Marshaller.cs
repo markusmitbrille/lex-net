e6877842e8d6f557c802150ebc9ed19bd5462fdb
Markus Rezai <markus.rezai@gmail.com>
2018-02-07 09:34:19 +0100

    Simplified serialization class hierarchy
    Extracted the private serializationmarshaller class from serializer bass class, thereby making the imarshaller interface obsolete. The only realy change here is that Serializer.Marshaller's setter is now internal instead of private, but that's okay, it's pretty obvious that this backreference to marshaller shouldn't be fucked with, within the LEX assembly at least.

--- LEX.NET/Serialization/Marshaller.cs
+++ LEX.NET/Serialization/Marshaller.cs
@@ -1,32 +1,52 @@
-﻿using System;
+﻿using Autrage.LEX.NET.Extensions;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using static Autrage.LEX.NET.DebugUtils;
 
 namespace Autrage.LEX.NET.Serialization
 {
-    public static class Marshaller
+    public class Marshaller
     {
+        #region Fields
+
+        private List<Serializer> serializers = new List<Serializer>();
+
+        #endregion Fields
+
+        #region Properties
+
+        public Encoding Encoding { get; set; } = Encoding.UTF8;
+        public Func<AssemblyName, Assembly> AssemblyResolver { get; set; }
+        public Func<Assembly, string, bool, Type> TypeResolver { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public Marshaller(params Serializer[] serializers)
+        {
+            foreach (Serializer serializer in serializers)
+            {
+                AddSerializer(serializer);
+            }
+        }
+
+        private Marshaller()
+        {
+        }
+
+        #endregion Constructors
+
         #region Methods
 
-        public static IMarshaller Create()
+        public static void Serialize(Stream stream, object instance, params Serializer[] serializers)
         {
-            return Serializer.CreateMarshaller();
-        }
-
-        public static IMarshaller Create(params Type[] serializers)
-        {
-            IMarshaller marshaller = Serializer.CreateMarshaller();
-            foreach (Type serializer in serializers)
-            {
-                marshaller.AddSerializer(serializer);
-            }
-
-            return marshaller;
-        }
-
-        public static void Serialize(Stream stream, object instance, params Type[] serializers)
-        {
-            IMarshaller marshaller = Serializer.CreateMarshaller();
-            foreach (Type serializer in serializers)
+            Marshaller marshaller = new Marshaller();
+            foreach (Serializer serializer in serializers)
             {
                 marshaller.AddSerializer(serializer);
             }
@@ -34,15 +54,10 @@ namespace Autrage.LEX.NET.Serialization
             marshaller.Serialize(stream, instance);
         }
 
-        public static void Serialize<T>(Stream stream, T instance, params Type[] serializers)
+        public static object Deserialize(Stream stream, params Serializer[] serializers)
         {
-            Serialize(stream, (object)instance, serializers);
-        }
-
-        public static object Deserialize(Stream stream, params Type[] serializers)
-        {
-            IMarshaller marshaller = Serializer.CreateMarshaller();
-            foreach (Type serializer in serializers)
+            Marshaller marshaller = new Marshaller();
+            foreach (Serializer serializer in serializers)
             {
                 marshaller.AddSerializer(serializer);
             }
@@ -50,7 +65,7 @@ namespace Autrage.LEX.NET.Serialization
             return marshaller.Deserialize(stream);
         }
 
-        public static T Deserialize<T>(Stream stream, params Type[] serializers)
+        public static T Deserialize<T>(Stream stream, params Serializer[] serializers)
         {
             if (Deserialize(stream, serializers) is T instance)
             {
@@ -60,6 +75,140 @@ namespace Autrage.LEX.NET.Serialization
             {
                 return default;
             }
+        }
+
+        public void Serialize(Stream stream, object instance)
+        {
+            stream.AssertNotNull();
+
+            if (instance == null)
+            {
+                stream.Write(false);
+                return;
+            }
+
+            string typeName = Cache.GetNameFrom(instance.GetType());
+            if (typeName == null)
+            {
+                Warning($"Could not retrieve name for {instance.GetType()} from cache!");
+                stream.Write(false);
+                return;
+            }
+
+            byte[] payloadBuffer;
+            using (MemoryStream payload = new MemoryStream())
+            {
+                Serializer serializer = serializers.FirstOrDefault(s => s.CanHandle(instance.GetType()));
+                if (serializer == null)
+                {
+                    Warning($"No suitable serializer specified for {instance.GetType()}!");
+                    stream.Write(false);
+                    return;
+                }
+
+                if (!serializer.Serialize(payload, instance))
+                {
+                    Warning($"{instance.GetType()} instance serialization not successful!");
+                    stream.Write(false);
+                    return;
+                }
+
+                payloadBuffer = payload.ToArray();
+            }
+
+            stream.Write(true);
+            stream.Write(typeName, Encoding);
+            stream.Write(payloadBuffer.Length);
+            stream.Write(payloadBuffer);
+        }
+
+        public void Serialize<T>(Stream stream, T instance)
+        {
+            Serialize(stream, (object)instance);
+        }
+
+        public object Deserialize(Stream stream)
+        {
+            stream.AssertNotNull();
+
+            bool? hasValue = stream.ReadBool();
+            if (hasValue == null)
+            {
+                Warning($"Could not read value indicator!");
+                return null;
+            }
+            if (hasValue == false)
+            {
+                return null;
+            }
+
+            string typeName = stream.ReadString(Encoding);
+            if (typeName == null)
+            {
+                Warning($"Could not read type name!");
+                return null;
+            }
+
+            Type type = Cache.GetTypeFrom(typeName, AssemblyResolver, TypeResolver);
+            if (type == null)
+            {
+                Warning($"Could not retrieve type for {typeName} from cache!");
+                return null;
+            }
+
+            int? payloadBufferLength = stream.ReadInt();
+            if (payloadBufferLength == null)
+            {
+                Warning($"Could not read {type} instance payload buffer length!");
+                return null;
+            }
+
+            byte[] payloadBuffer = stream.Read(payloadBufferLength.Value);
+            if (payloadBuffer == null)
+            {
+                Warning($"Could not read {type} instance payload!");
+                return null;
+            }
+
+            object result = null;
+            using (MemoryStream payload = new MemoryStream(payloadBuffer))
+            {
+                Serializer serializer = serializers.FirstOrDefault(s => s.CanHandle(type));
+                if (serializer == null)
+                {
+                    Warning($"No suitable serializer specified for {type}!");
+                    return null;
+                }
+
+                result = serializer.Deserialize(payload, type);
+            }
+
+            if (type.IsInstanceOfType(result))
+            {
+                return result;
+            }
+            else
+            {
+                return type.GetDefault();
+            }
+        }
+
+        public T Deserialize<T>(Stream stream)
+        {
+            if (Deserialize(stream) is T instance)
+            {
+                return instance;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        private void AddSerializer(Serializer serializer)
+        {
+            serializer.Marshaller = this;
+            serializers.Add(serializer);
         }
 
         #endregion Methods
--- LEX.NET/Serialization/Serializer.cs
+++ LEX.NET/Serialization/Serializer.cs
@@ -1,11 +1,5 @@
-﻿using Autrage.LEX.NET.Extensions;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using static Autrage.LEX.NET.DebugUtils;
 
 namespace Autrage.LEX.NET.Serialization
 {
@@ -13,7 +7,7 @@ namespace Autrage.LEX.NET.Serialization
     {
         #region Properties
 
-        public IMarshaller Marshaller { get; private set; }
+        public Marshaller Marshaller { get; internal set; }
 
         #endregion Properties
 
@@ -25,183 +19,6 @@ namespace Autrage.LEX.NET.Serialization
 
         public abstract object Deserialize(Stream stream, Type type);
 
-        internal static IMarshaller CreateMarshaller() => new SerializationMarshaller();
-
         #endregion Methods
-
-        #region Classes
-
-        private class SerializationMarshaller : IMarshaller
-        {
-            #region Fields
-
-            private List<Serializer> serializers = new List<Serializer>();
-
-            #endregion Fields
-
-            #region Properties
-
-            public Encoding Encoding { get; set; } = Encoding.UTF8;
-            public Func<AssemblyName, Assembly> AssemblyResolver { get; set; }
-            public Func<Assembly, string, bool, Type> TypeResolver { get; set; }
-
-            #endregion Properties
-
-            #region Methods
-
-            public Serializer AddSerializer(Type serializerType)
-            {
-                if (!serializerType.IsSubclassOf(typeof(Serializer))) return null;
-                if (serializers.Any(s => s.GetType() == serializerType)) return null;
-
-                Serializer serializer = (Serializer)Activator.CreateInstance(serializerType, true);
-                serializer.Marshaller = this;
-                serializers.Add(serializer);
-
-                return serializer;
-            }
-
-            public T AddSerializer<T>() where T : Serializer, new()
-            {
-                if (serializers.Any(s => s is T)) return default;
-
-                T serializer = new T() { Marshaller = this };
-                serializers.Add(serializer);
-
-                return serializer;
-            }
-
-            public void Serialize(Stream stream, object instance)
-            {
-                stream.AssertNotNull();
-
-                if (instance == null)
-                {
-                    stream.Write(false);
-                    return;
-                }
-
-                string typeName = Cache.GetNameFrom(instance.GetType());
-                if (typeName == null)
-                {
-                    Warning($"Could not retrieve name for {instance.GetType()} from cache!");
-                    stream.Write(false);
-                    return;
-                }
-
-                byte[] payloadBuffer;
-                using (MemoryStream payload = new MemoryStream())
-                {
-                    Serializer serializer = serializers.FirstOrDefault(s => s.CanHandle(instance.GetType()));
-                    if (serializer == null)
-                    {
-                        Warning($"No suitable serializer specified for {instance.GetType()}!");
-                        stream.Write(false);
-                        return;
-                    }
-
-                    if (!serializer.Serialize(payload, instance))
-                    {
-                        Warning($"{instance.GetType()} instance serialization not successful!");
-                        stream.Write(false);
-                        return;
-                    }
-
-                    payloadBuffer = payload.ToArray();
-                }
-
-                stream.Write(true);
-                stream.Write(typeName, Encoding);
-                stream.Write(payloadBuffer.Length);
-                stream.Write(payloadBuffer);
-            }
-
-            public void Serialize<T>(Stream stream, T instance)
-            {
-                Serialize(stream, (object)instance);
-            }
-
-            public object Deserialize(Stream stream)
-            {
-                stream.AssertNotNull();
-
-                bool? hasValue = stream.ReadBool();
-                if (hasValue == null)
-                {
-                    Warning($"Could not read value indicator!");
-                    return null;
-                }
-                if (hasValue == false)
-                {
-                    return null;
-                }
-
-                string typeName = stream.ReadString(Encoding);
-                if (typeName == null)
-                {
-                    Warning($"Could not read type name!");
-                    return null;
-                }
-
-                Type type = Cache.GetTypeFrom(typeName, AssemblyResolver, TypeResolver);
-                if (type == null)
-                {
-                    Warning($"Could not retrieve type for {typeName} from cache!");
-                    return null;
-                }
-
-                int? payloadBufferLength = stream.ReadInt();
-                if (payloadBufferLength == null)
-                {
-                    Warning($"Could not read {type} instance payload buffer length!");
-                    return null;
-                }
-
-                byte[] payloadBuffer = stream.Read(payloadBufferLength.Value);
-                if (payloadBuffer == null)
-                {
-                    Warning($"Could not read {type} instance payload!");
-                    return null;
-                }
-
-                object result = null;
-                using (MemoryStream payload = new MemoryStream(payloadBuffer))
-                {
-                    Serializer serializer = serializers.FirstOrDefault(s => s.CanHandle(type));
-                    if (serializer == null)
-                    {
-                        Warning($"No suitable serializer specified for {type}!");
-                        return null;
-                    }
-
-                    result = serializer.Deserialize(payload, type);
-                }
-
-                if (type.IsInstanceOfType(result))
-                {
-                    return result;
-                }
-                else
-                {
-                    return type.GetDefault();
-                }
-            }
-
-            public T Deserialize<T>(Stream stream)
-            {
-                if (Deserialize(stream) is T instance)
-                {
-                    return instance;
-                }
-                else
-                {
-                    return default;
-                }
-            }
-
-            #endregion Methods
-        }
-
-        #endregion Classes
     }
 }
