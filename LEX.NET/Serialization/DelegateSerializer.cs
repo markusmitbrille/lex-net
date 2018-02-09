a0679750e8d8736a94f4e0e2ba8fb9fc959d674a
Markus Rezai <markus.rezai@gmail.com>
2018-02-09 13:53:41 +0100

    First try at implementing a delegate serializer
    Serialization has to exclude invalid identifier produced by anonymous methods.
    This version has to be revised: RefTypeSerializer won't work as delegate instantiation has to be treated specially, via static methods of MulticastDelegate. Plus several contingencies are not tested (generic parameters, ambigous matches)

---
+++ LEX.NET/Serialization/DelegateSerializer.cs
@@ -0,0 +1,75 @@
+﻿using Autrage.LEX.NET.Extensions;
+using Microsoft.CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static Autrage.LEX.NET.DebugUtils;
+
+namespace Autrage.LEX.NET.Serialization
+{
+    class DelegateSerializer : ReferenceTypeSerializer
+    {
+        private static CSharpCodeProvider Provider { get; } = new CSharpCodeProvider();
+
+        public override bool CanHandle(Type type) => typeof(MulticastDelegate).IsAssignableFrom(type);
+
+        protected override bool SerializePayload(Stream stream, object instance)
+        {
+            MulticastDelegate multiDel = (MulticastDelegate)instance;
+            var methods =
+                from del in multiDel.GetInvocationList()
+                let method = del.Method
+                where Provider.IsValidIdentifier(method.Name)
+                select method;
+
+            stream.Write(methods.Count());
+            foreach (var method in methods)
+            {
+                stream.Write(Cache.GetNameFrom(method.DeclaringType));
+                stream.Write(method.Name);
+            }
+
+            return true;
+        }
+
+        protected override bool DeserializePayload(Stream stream, object instance)
+        {
+            MulticastDelegate multiDel = (MulticastDelegate)instance;
+
+            int? methodCount = stream.ReadInt();
+            if(methodCount==null)
+            {
+                Warning("Could not read multicast delegate method count!");
+                return false;
+            }
+
+            for (int i = 0; i < methodCount.Value; i++)
+            {
+                string declaringType = stream.ReadString();
+                if (declaringType == null)
+                {
+                    Warning("Could not read multicast delegate method declaring type!");
+                    return false;
+                }
+
+                string name = stream.ReadString();
+                if (name == null)
+                {
+                    Warning("Could not read multicast delegate method name!");
+                    return false;
+                }
+
+                Type type = Cache.GetTypeFrom(declaringType, Marshaller.AssemblyResolver, Marshaller.TypeResolver);
+                MethodInfo method = type.GetMethod(name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+                // Not working like this
+            }
+
+            return true;
+        }
+    }
+}
