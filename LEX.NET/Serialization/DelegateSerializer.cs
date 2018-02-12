1ceec4f1db09a415bf4aa18b475099e5e38db7d4
Markus Rezai <markus.rezai@gmail.com>
2018-02-12 10:38:39 +0100

    Implemented delegate serializer as its own variation of RefSerializer

--- LEX.NET/Serialization/DelegateSerializer.cs
+++ LEX.NET/Serialization/DelegateSerializer.cs
@@ -5,71 +5,200 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using static Autrage.LEX.NET.DebugUtils;
 
 namespace Autrage.LEX.NET.Serialization
 {
-    class DelegateSerializer : ReferenceTypeSerializer
+    public sealed class DelegateSerializer : Serializer
     {
+        #region Fields
+
+        private Dictionary<MulticastDelegate, long> referenceIDs = new Dictionary<MulticastDelegate, long>(new ReferenceComparer());
+        private long nextID = 0;
+
+        private Dictionary<long, MulticastDelegate> references = new Dictionary<long, MulticastDelegate>();
+
+        #endregion Fields
+
+        #region Properties
+
         private static CSharpCodeProvider Provider { get; } = new CSharpCodeProvider();
+
+        #endregion Properties
+
+        #region Methods
 
         public override bool CanHandle(Type type) => typeof(MulticastDelegate).IsAssignableFrom(type);
 
-        protected override bool SerializePayload(Stream stream, object instance)
+        public override bool Serialize(Stream stream, object instance)
         {
+            stream.AssertNotNull();
+            instance.AssertNotNull();
+
+            Type type = instance.GetType();
+            if (!CanHandle(type))
+            {
+                Warning($"{nameof(DelegateSerializer)} cannot handle type {type}!");
+                return false;
+            }
+
             MulticastDelegate multiDel = (MulticastDelegate)instance;
+
+            if (referenceIDs.ContainsKey(multiDel))
+            {
+                stream.Write(referenceIDs[multiDel]);
+                stream.Write(false);
+                return true;
+            }
+
+            long referenceID = nextID++;
+            referenceIDs[multiDel] = referenceID;
+            stream.Write(referenceID);
+            stream.Write(true);
+
             var methods =
                 from del in multiDel.GetInvocationList()
                 let method = del.Method
                 where Provider.IsValidIdentifier(method.Name)
-                select method;
+                let declaringTypeName = Cache.GetNameFrom(method.DeclaringType)
+                where declaringTypeName != null
+                let paramTypeNames =
+                    from parameter in method.GetParameters()
+                    let paramTypeName = Cache.GetNameFrom(parameter.ParameterType)
+                    select paramTypeName
+                where !paramTypeNames.Contains(null)
+                select new { name = method.Name, declaringTypeName, paramTypeNames };
 
             stream.Write(methods.Count());
             foreach (var method in methods)
             {
-                stream.Write(Cache.GetNameFrom(method.DeclaringType));
-                stream.Write(method.Name);
+                stream.Write(method.name, Marshaller.Encoding);
+                stream.Write(method.declaringTypeName, Marshaller.Encoding);
+
+                stream.Write(method.paramTypeNames.Count());
+                foreach (string paramTypeName in method.paramTypeNames)
+                {
+                    stream.Write(paramTypeName, Marshaller.Encoding);
+                }
             }
 
             return true;
         }
 
-        protected override bool DeserializePayload(Stream stream, object instance)
+        public override object Deserialize(Stream stream, Type type)
         {
-            MulticastDelegate multiDel = (MulticastDelegate)instance;
+            stream.AssertNotNull();
+            type.AssertNotNull();
 
-            int? methodCount = stream.ReadInt();
-            if(methodCount==null)
+            if (!CanHandle(type))
             {
-                Warning("Could not read multicast delegate method count!");
-                return false;
+                Warning($"{nameof(DelegateSerializer)} cannot handle type {type}!");
+                return null;
             }
 
-            for (int i = 0; i < methodCount.Value; i++)
+            long? referenceID = stream.ReadLong();
+            if (referenceID == null)
             {
-                string declaringType = stream.ReadString();
-                if (declaringType == null)
+                Warning($"Could not read {type} reference ID!");
+                return null;
+            }
+
+            MulticastDelegate instance = references.GetValueOrDefault(referenceID.Value);
+
+            bool? hasPayload = stream.ReadBool();
+            if (hasPayload == null)
+            {
+                Warning($"Could not read {type} payload indicator!");
+                return null;
+            }
+            if (hasPayload == false)
+            {
+                if (instance == null)
                 {
-                    Warning("Could not read multicast delegate method declaring type!");
-                    return false;
+                    Warning($"Neither payload nor already restored reference found for {type}!");
                 }
 
-                string name = stream.ReadString();
+                return instance;
+            }
+
+            int? methodCount = stream.ReadInt();
+            if (methodCount == null)
+            {
+                Warning("Could not read multicast delegate method count!");
+                return null;
+            }
+
+            Delegate[] dels = new Delegate[methodCount.Value];
+            for (int i = 0; i < methodCount.Value; i++)
+            {
+                string name = stream.ReadString(Marshaller.Encoding);
                 if (name == null)
                 {
                     Warning("Could not read multicast delegate method name!");
-                    return false;
+                    return null;
                 }
 
-                Type type = Cache.GetTypeFrom(declaringType, Marshaller.AssemblyResolver, Marshaller.TypeResolver);
-                MethodInfo method = type.GetMethod(name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                string declaringTypeName = stream.ReadString(Marshaller.Encoding);
+                if (declaringTypeName == null)
+                {
+                    Warning("Could not read multicast delegate method declaring type!");
+                    return null;
+                }
 
-                // Not working like this
+                Type declaringType = Cache.GetTypeFrom(declaringTypeName, Marshaller.AssemblyResolver, Marshaller.TypeResolver);
+                if (declaringType == null)
+                {
+                    Warning($"Could not retrieve type for {declaringTypeName} from cache!");
+                    return null;
+                }
+
+                int? paramTypeNameCount = stream.ReadInt();
+                if (paramTypeNameCount == null)
+                {
+                    Warning("Could not read multicast delegate method parameter type name count!");
+                    return null;
+                }
+
+                Type[] paramTypes = new Type[paramTypeNameCount.Value];
+                for (int j = 0; j < paramTypeNameCount.Value; j++)
+                {
+                    string paramTypeName = stream.ReadString(Marshaller.Encoding);
+                    if (paramTypeName == null)
+                    {
+                        Warning("Could not read multicast delegate method parameter type name!");
+                        return null;
+                    }
+
+                    Type paramType = Cache.GetTypeFrom(paramTypeName, Marshaller.AssemblyResolver, Marshaller.TypeResolver);
+                    if (paramType == null)
+                    {
+                        Warning($"Could not retrieve type for {paramTypeName} from cache!");
+                        return null;
+                    }
+
+                    paramTypes[j] = paramType;
+                }
+
+                const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+                MethodInfo method = declaringType.GetMethod(name, bindingFlags, null, paramTypes, null);
+                if (method == null)
+                {
+                    Warning($"Could not find method {name} on type {declaringType}!");
+                    return null;
+                }
+
+                dels[i] = Delegate.CreateDelegate(type, method);
             }
 
-            return true;
+            if (instance == null)
+            {
+                instance = (MulticastDelegate)Delegate.Combine(dels);
+                references[referenceID.Value] = instance;
+            }
+
+            return instance;
         }
+
+        #endregion Methods
     }
 }
